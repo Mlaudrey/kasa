f2b945be0b709efe5a78c1edaee4b1ca6dc31ee5
AudreyMl <audreytraining33@gmail.com>
2024-09-20 09:30:51 +0200

    import image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logements from './data/logements.json'; // Import des données JSON
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import Pages2 from './components/Pages2/Pages2';
 
 function App() {
+  console.log(logements); // Pour vérifier si les données JSON sont bien importées
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pages2 />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Pages2" element={<Pages2 />} />
+      </Routes>
     </div>
   );
 }
