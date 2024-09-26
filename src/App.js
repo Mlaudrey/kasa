c53d1e0244433091b64cd27988ce5c29165cc2d4
AudreyMl <audreytraining33@gmail.com>
2024-09-26 12:21:18 +0200

    modification component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React from "react";
+import React, { createContext } from "react";
 import './App.scss';
-import Header from "./compenents/Header/Header.js"
-import Footer from "./compenents/Footer/Footer.js"
-import Home from "./compenents/Home/Home.js";
+import Header from "./component/Header/Header.js";
+import Footer from "./component/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import NotFound from './404/Not-found.js';
-import InfosLogement from './compenents/Logements/logements.js';
+import InfosLogement from './component/Logements/logements.js';
+import Pages2 from './components/Pages2/Pages2.js';
 
-import Pages2 from './compenents/Pages2/Pages2.js';
+// Créer le contexte MyContext
+const MyContext = createContext();
 
 const App = () => {
   const basenameValue = '/APP'; 
 
   return (
-   
-   <BrowserRouter>
-   
-      <div className='main-container'>
-      <Header />
-      <main>
-      <Routes> 
-         <Route path="/InfosLogement/:id" element={<InfosLogement  />} />
-         <Route path="/Pages2" element={<Pages2 />} />
-         <Route path="/" element={<Home />} />
-         <Route path="/Not-found" element={<NotFound />} />
-         <Route path="/*" element={<Navigate replace to="/Not-found" />} />
-      </Routes>
-      </main>
-     </div>
-     <Footer />
-   
-    </BrowserRouter>
-    
+    <MyContext.Provider value={{ basename: basenameValue }}>
+      <BrowserRouter basename={basenameValue}>
+        <div className='main-container'>
+          <Header />
+          <main>
+            <Routes> 
+              <Route path="/InfosLogement/:id" element={<InfosLogement />} />
+              <Route path="/Pages2" element={<Pages2 />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/Not-found" element={<NotFound />} />
+              <Route path="/*" element={<Navigate replace to="/Not-found" />} />
+            </Routes>
+          </main>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </MyContext.Provider>
   );
 };
-
 
 export default App;
