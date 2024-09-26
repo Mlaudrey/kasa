b27c3ecaa4b1efadbcb2db047177e8e629b24772
AudreyMl <audreytraining33@gmail.com>
2024-09-26 13:55:40 +0200

    création de host

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React from "react";
 import './App.scss';
 import Header from "./component/Header/Header.js";
 import Footer from "./component/Footer/Footer.js";
@@ -6,31 +6,31 @@ import Home from "./component/Home/Home.js";
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import NotFound from './404/Not-found.js';
 import InfosLogement from './component/Logements/logements.js';
-import Pages2 from './components/Pages2/Pages2.js';
-
-// Créer le contexte MyContext
-const MyContext = createContext();
+import MyContext from './MyContext.js';
+import Pages2 from './component/Pages2/Pages2.js';
 
 const App = () => {
   const basenameValue = '/APP'; 
 
   return (
     <MyContext.Provider value={{ basename: basenameValue }}>
-      <BrowserRouter basename={basenameValue}>
-        <div className='main-container'>
-          <Header />
-          <main>
-            <Routes> 
-              <Route path="/InfosLogement/:id" element={<InfosLogement />} />
-              <Route path="/Pages2" element={<Pages2 />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/Not-found" element={<NotFound />} />
-              <Route path="/*" element={<Navigate replace to="/Not-found" />} />
-            </Routes>
-          </main>
-        </div>
-        <Footer />
-      </BrowserRouter>
+   <BrowserRouter>
+   
+      <div className='main-container'>
+      <Header />
+      <main>
+      <Routes> 
+         <Route path="/InfosLogement/:id" element={<InfosLogement  />} />
+         <Route path="/Pages2" element={<Pages2 />} />
+         <Route path="/" element={<Home />} />
+         <Route path="/Not-found" element={<NotFound />} />
+         <Route path="/*" element={<Navigate replace to="/Not-found" />} />
+      </Routes>
+      </main>
+     </div>
+     <Footer />
+   
+    </BrowserRouter>
     </MyContext.Provider>
   );
 };
